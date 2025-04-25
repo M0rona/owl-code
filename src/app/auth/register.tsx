db0c7e547225da176960b1d969569dbc7b2843ca
M0rona <gabrielmorona0229@gmail.com>
2025-04-25 19:23:19 -0300

    feat(auth): add profile image upload functionality in registration

--- src/app/auth/register.tsx
+++ src/app/auth/register.tsx
@@ -2,7 +2,7 @@ import { Input } from "@/components/ui/input";
 import { AuthLayout } from "./components/authLayout";
 import { Button, buttonVariants } from "@/components/ui/button";
 import { Link, useNavigate } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { auth } from "@/config/firebase";
 import { FirebaseError } from "firebase/app";
@@ -11,12 +11,16 @@ import { Loader2 } from "lucide-react";
 
 export function RegisterPage() {
   const [isLoading, setIsLoading] = useState(false);
+  const [image, setImage] = useState<File | null>(null);
+  const [imageUrl, setImageUrl] = useState<string | null>(null);
   const navigate = useNavigate();
+  const inputImageRef = useRef<HTMLInputElement>(null);
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     const formData = new FormData(e.currentTarget);
+    // const profileImage = formData.get("profileImage") as string;
     const userName = formData.get("username") as string;
     const email = formData.get("email") as string;
     const password = formData.get("password") as string;
@@ -42,8 +46,54 @@ export function RegisterPage() {
 
     setIsLoading(false);
   };
+
+  // TODO: Ver sobre inserir a foto de perfil
+  useEffect(() => {
+    let objectUrl: string | null = null;
+    if (image) {
+      objectUrl = URL.createObjectURL(image);
+      setImageUrl(objectUrl);
+    } else {
+      setImageUrl(null);
+    }
+    return () => {
+      if (objectUrl) {
+        URL.revokeObjectURL(objectUrl);
+      }
+    };
+  }, [image]);
+
   return (
     <AuthLayout label="Registre-se" onSubmit={handleSubmit}>
+      <div className="flex justify-center mb-5">
+        <div
+          className="size-32 rounded-full bg-background flex items-center justify-center p-2 cursor-pointer"
+          onClick={() => inputImageRef.current?.click()}
+        >
+          <span className="text-center text-xs">Insira sua foto de perfil</span>
+          {imageUrl && (
+            <img
+              src={imageUrl}
+              alt="foto-de-perfil"
+              className="absolute size-32 rounded-full"
+            />
+          )}
+        </div>
+      </div>
+
+      <input
+        ref={inputImageRef}
+        type="file"
+        name="profileImage"
+        onChange={(e) => {
+          if (e.target.files && e.target.files[0]) {
+            setImage(e.target.files[0]);
+          }
+        }}
+        hidden
+        accept="image/*"
+      />
+
       <Input
         placeholder="Nome de usuário"
         type="text"
